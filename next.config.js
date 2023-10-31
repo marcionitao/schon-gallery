ef7f12dcef6d61c6c14c9624e606680d65fcc6c4
Márcio <marcionitao@gmail.com>
2023-10-31 17:25:04 +0100

    add ImgContainer and config. remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/photos/**',
+      },
+    ],
+  },
   experimental: {
     serverActions: true,
   },
